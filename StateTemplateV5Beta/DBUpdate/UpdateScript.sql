28973d98c00adec68e8277ad3ff737a34624a864
MatthewLoller <Matthewloller@gmail.com>
2017-08-16 16:31:02 -0700

    Added the deleted js file as a txt file, added update script for the db

---
+++ StateTemplateV5Beta/DBUpdate/UpdateScript.sql
@@ -0,0 +1,26 @@
+﻿USE [CSL]
+GO
+
+/****** Object:  Table [dbo].[AddBlog]    Script Date: 8/16/2017 4:29:06 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AddBlog](
+	[BlogID] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [varchar](100) NOT NULL,
+	[Description] [varchar](max) NOT NULL,
+	[Date] [datetime] NOT NULL,
+ CONSTRAINT [PK_AddBlogID] PRIMARY KEY CLUSTERED 
+(
+	[BlogID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[AddBlog] ADD  CONSTRAINT [DF_AddBlog_Date]  DEFAULT (getdate()) FOR [Date]
+GO
+
+--4:30 8/16/17 matt ran to here
