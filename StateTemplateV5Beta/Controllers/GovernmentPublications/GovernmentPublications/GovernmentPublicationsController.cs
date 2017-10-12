4e7ec58dd83c847ea721ac753ff06560cd9a93fa
Loller, Matthew Taylor <matthewloller@csus.edu>
2017-10-11 20:44:17 -0700

    merge

---
+++ StateTemplateV5Beta/Controllers/GovernmentPublications/GovernmentPublications/GovernmentPublicationsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateTemplateV5Beta.Controllers.GovernmentPublications
+{
+    [RoutePrefix("government-publications")]
+    public class GovernmentPublicationsController : Controller
+    {
+        // GET: GovernmentPublications
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View("GovernmentPublications");
+        }
+
+        // GET: SLAA
+        [Route("slaa")]
+        public ActionResult SLAA()
+        {
+            return View();
+        }
+
+        // GET: state-document-depositories
+        [Route("state-document-depositories")]
+        public ActionResult StateDocumentDepositories()
+        {
+            return View();
+        }
+    }
+}
---
+++ StateTemplateV5Beta/Controllers/GovernmentPublications/GovernmentPublications/StateDocumentDepositoryProgram/StateDocumentDepositoryProgramController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateTemplateV5Beta.Controllers.GovernmentPublications.GovernmentPublications.StateDocumentDepositoryProgram
+{
+    [RoutePrefix("government-publications/state-document-depository-program")]
+    public class StateDocumentDepositoryProgramController : Controller
+    {
+        // GET: StateDocumentDepositoryProgram
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View("~/Views/GovernmentPublications/StateDocumentDepositoryProgram/StateDocumentDepositoryProgram.cshtml");
+        }
+
+        // GET: depositories
+        [Route("depositories")]
+        public ActionResult Depositories()
+        {
+            return View("~/Views/GovernmentPublications/StateDocumentDepositoryProgram/Depositories.cshtml");
+        }
+
+        // GET: policies
+        [Route("policies")]
+        public ActionResult Policies()
+        {
+            return View("~/Views/GovernmentPublications/StateDocumentDepositoryProgram/Policies.cshtml");
+        }
+    }
+}
