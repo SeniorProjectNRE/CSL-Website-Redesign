513ee12fb9fa19ed4f152b4f5ebf07136a88f996
Loller, Matthew Taylor <matthewloller@csus.edu>
2017-10-16 21:37:17 -0700

    added bunch of stuff for N teir app yada yada

---
+++ CSLBusinessObjects/ConfigModels/DBConnectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLBusinessObjects.ConfigModels
+{
+    public class DBConnectionConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
