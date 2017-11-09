a1813f82e777878e83429a7604bf0458243d66da
MatthewLoller <Matthewloller@gmail.com>
2017-11-08 20:30:31 -0800

    added test project

--- StateTemplateV5Beta/Controllers/WorkWithUs/Jobs/Librarian/LibrarianController.cs
+++ StateTemplateV5Beta/Controllers/WorkWithUs/Jobs/Librarian/LibrarianController.cs
@@ -45,73 +45,80 @@ namespace StateTemplateV5Beta.Controllers.WorkWithUs.Jobs.Librarian
         [Route("apply")]
         public ActionResult Apply(LibrarianModel model, HttpPostedFileBase file)
         {
-            if(file != null)
+            try
             {
-                string ext = Path.GetExtension(file.FileName);
-                if (ext == ".pdf" || ext == ".doc" || ext == ".docx")
+                if (file != null)
                 {
-                    model.ResumeUpload = file;
+                    string ext = Path.GetExtension(file.FileName);
+                    if (ext == ".pdf" || ext == ".doc" || ext == ".docx")
+                    {
+                        model.ResumeUpload = file;
+                    }
+                    else model.ResumeUpload = null;
                 }
-                else model.ResumeUpload = null;
-            }
-            
 
-            if (!ModelState.IsValid)
-            {
-                if (model.IsLibrarian == true && model.IsSeniorLibrarian == false)
-                {
-                    ModelState.Remove("Q7");
-                    ModelState.Remove("NameQ7");
-                    ModelState.Remove("EmailQ7");
-                    ModelState.Remove("PhoneNumQ7");
-                    ModelState.Remove("Q8");
-                    ModelState.Remove("NameQ8");
-                    ModelState.Remove("EmailQ8");
-                    ModelState.Remove("PhoneNumQ8");
-                }
+
                 if (!ModelState.IsValid)
+                {
+                    if (model.IsLibrarian == true && model.IsSeniorLibrarian == false)
+                    {
+                        ModelState.Remove("Q7");
+                        ModelState.Remove("NameQ7");
+                        ModelState.Remove("EmailQ7");
+                        ModelState.Remove("PhoneNumQ7");
+                        ModelState.Remove("Q8");
+                        ModelState.Remove("NameQ8");
+                        ModelState.Remove("EmailQ8");
+                        ModelState.Remove("PhoneNumQ8");
+                    }
+                    if (!ModelState.IsValid)
+                    {
+                        return View("~/Views/WorkWithUs/Jobs/Librarian/Apply.cshtml", model);
+                    }
+                }
+
+                var response = Request["g-recaptcha-response"];
+                string secretKey = "6Lf0XzYUAAAAAEHDxHcYouICuNOMph90Gc1XTOaT";
+                var client = new WebClient();
+                var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, response));
+                var obj = JObject.Parse(result);
+                var status = (bool)obj.SelectToken("success");
+                ViewBag.Message = status ? "Google reCaptcha validation success" : "Google reCaptcha validation failed";
+                model.IsCaptcha = status;
+
+                if (model.IsCaptcha == false)
                 {
                     return View("~/Views/WorkWithUs/Jobs/Librarian/Apply.cshtml", model);
                 }
-            }
 
-            var response = Request["g-recaptcha-response"];
-            string secretKey = "6Lf0XzYUAAAAAEHDxHcYouICuNOMph90Gc1XTOaT";
-            var client = new WebClient();
-            var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, response));
-            var obj = JObject.Parse(result);
-            var status = (bool)obj.SelectToken("success");
-            ViewBag.Message = status ? "Google reCaptcha validation success" : "Google reCaptcha validation failed";
-            model.IsCaptcha = status;
+                string pdfLibrarianTemplate = "~/Content/StateTemplate/pdf/ExamPDFTemplates/LibAppFinal.pdf";
+                string pdfSeniorLibrarianTemplate = "~/Content/StateTemplate/pdf/ExamPDFTemplates/LibAppFinal.pdf";
+                string newFile = "~/Content/StateTemplate/pdf/ExamPDFTemplates/" + model.Name + "_" + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".pdf";
 
-            if (model.IsCaptcha == false)
-            {
+                if (model.IsLibrarian == true && model.IsSeniorLibrarian == false)
+                {
+                    _examService.FillLibrarianExam(model, pdfLibrarianTemplate, newFile);
+                }
+                else if (model.IsSeniorLibrarian == true)
+                {
+                    //_examService.FillSeniorLibrarianExam(model, pdfSeniorLibrarianTemplate, newFile);
+                }
+
+                bool res = _emailService.SendLibrarianExamEmail(newFile, model);
+
+                if (System.IO.File.Exists(HostingEnvironment.MapPath(newFile)))
+                {
+                    System.IO.File.Delete(HostingEnvironment.MapPath(newFile));
+                }
+
+                ModelState.Clear();
+                model.Success = res;
                 return View("~/Views/WorkWithUs/Jobs/Librarian/Apply.cshtml", model);
             }
-
-            string pdfLibrarianTemplate = "~/Content/StateTemplate/pdf/ExamPDFTemplates/LibAppFinal.pdf";
-            string pdfSeniorLibrarianTemplate = "~/Content/StateTemplate/pdf/ExamPDFTemplates/LibAppFinal.pdf";
-            string newFile = "~/Content/StateTemplate/pdf/ExamPDFTemplates/" + model.Name + "_" + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".pdf";
-
-            if (model.IsLibrarian == true && model.IsSeniorLibrarian == false)
+            catch (Exception ex)
             {
-                _examService.FillLibrarianExam(model, pdfLibrarianTemplate, newFile);
+                return RedirectToAction("index", "error");
             }
-            else if (model.IsSeniorLibrarian == true)
-            {
-                //_examService.FillSeniorLibrarianExam(model, pdfSeniorLibrarianTemplate, newFile);
-            }
-
-            bool res = _emailService.SendLibrarianExamEmail(newFile, model);
-
-            if (System.IO.File.Exists(HostingEnvironment.MapPath(newFile)))
-            {
-                System.IO.File.Delete(HostingEnvironment.MapPath(newFile));
-            }
-
-            ModelState.Clear();
-            model.Success = res;
-            return View("~/Views/WorkWithUs/Jobs/Librarian/Apply.cshtml", model);
         }
 
         protected virtual bool IsFileLocked(FileInfo file)
