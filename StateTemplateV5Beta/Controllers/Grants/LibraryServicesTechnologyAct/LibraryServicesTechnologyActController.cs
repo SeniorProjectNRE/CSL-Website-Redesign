af36d68612e54cbb8d6dfdf10d922af0d1abcd0b
nreuser2 <nreuser2@gmail.com>
2017-11-19 12:41:15 -0800

    Adding /grants/library-services-technology-act/manage and /services/to-libraries/library-staff-education-program/ views. Updated necessary controllers

---
+++ StateTemplateV5Beta/Controllers/Grants/LibraryServicesTechnologyAct/LibraryServicesTechnologyActController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateTemplateV5Beta.Controllers.Grants.LibraryServicesTechnologyAct
+{
+    [RoutePrefix("grants/library-services-technology-act")]
+    public class LibraryServicesTechnologyActController : Controller
+    {
+        // GET: LibraryServicesTechnologyAct
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View("~/Views/Grants/LibraryServicesTechnologyAct.cshtml");
+        }
+
+        // GET: manage
+        [Route("manage")]
+        public ActionResult Manage()
+        {
+            return View("~/Views/Grants/LibraryServicesTechnologyAct/Manage.cshtml");
+        }
+    }
+}
